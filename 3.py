a6adc8aa4731aa6c8b40f45c8d9b9c8ea04835ec
nae0 <81555974+nae0@users.noreply.github.com>
2022-04-17 14:35:44 +0900

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,29 +1,24 @@
 from tkinter import *
-from tkinter.filedialog import *
-
-
-def keyUp(event) :
-    photo = PhotoImage(file = filename)
-    photo = photo.zoom(2, 2)
-    pLabel.configure(image = photo)
-    pLabel.image = photo
-
-
-def keyDown(event) :
-    photo = PhotoImage(file = filename)
-    photo = photo.subsample(2, 2)
-    pLabel.configure(image = photo)
-    pLabel.image = photo
-
+from tkinter import ttk
 
 window = Tk()
-window.geometry("500x500")
-window.title("이미지 확대 축소")
-filename = "python/dog7.gif"
-photo = PhotoImage(file = filename)
-pLabel = Label(window, image = photo)
-pLabel.pack()
+window.iconbitmap('python.ico')
 
-window.bind("<Up>", keyUp)
-window.bind("<Down>", keyDown)
+baseTab = ttk.Notebook(window)
+
+tabDog = ttk.Frame(baseTab)
+baseTab.add(tabDog, text='강아지')
+tabCat = ttk.Frame(baseTab)
+baseTab.add(tabCat, text = '고양이')
+
+baseTab.pack(expand=1, fill="both")
+
+photoDog = PhotoImage(file = "python/dog7.gif")
+labelDog = Label(tabDog, image = photoDog)
+labelDog.pack()
+
+photoCat = PhotoImage(file = "python/cat5.gif")
+labelCat = Label(tabCat, image = photoCat)
+labelCat.pack()
+
 window.mainloop()
